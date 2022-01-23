7fe5f1ee39d781a9f346b7bc1ad2bbf6ab043887
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-23 03:15:57 +0300

    Refactor

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,23 +1,31 @@
-import React from "react"
-import { useSelector } from "react-redux"
+import React from "react";
+import {Link} from 'react-router-dom'
+import { useSelector } from "react-redux";
 
-const ProductComponent = () =>{
-    const products = useSelector((state) => state.allProducts.products)
-    const {id, title} = products[0]
+
+const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const renderedList = products.map((product) => {
+    const { id, title, image, price, category } = product;
     return (
-        <div className="four wide column">
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image">
-                        <div className="content">
-                            <div className="header">
-                                {title}
-                            </div>
-                        </div>    
-                    </div>
-                </div>
+        <div className="four wide column" key={id}>
+            <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">ksh {price}</div>
+                <div className="meta">{category}</div>
+              </div>
             </div>
-        </div>
-    )
-}
-export default ProductComponent
+          </div>
+          </Link>
+      </div>
+    );
+  });
+  return <>{renderedList}</>;
+};
+export default ProductComponent;
